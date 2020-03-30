9a097f5831e1b33505d1595ff847e2e0d01d8918
luca-de-martini <10565376@polimi.it>
2020-03-30 15:54:16 +0200

    Change test to allow mvn test to function correctly

--- src/test/java/it/polimi/ingsw/model/action/ActionTest.java
+++ src/test/java/it/polimi/ingsw/model/action/ActionTest.java
@@ -2,42 +2,34 @@ package it.polimi.ingsw.model.action;
 
 import it.polimi.ingsw.model.board.Board;
 import it.polimi.ingsw.model.board.Coordinate;
+import it.polimi.ingsw.model.board.InvalidBuildException;
 import it.polimi.ingsw.model.board.InvalidMoveException;
 import it.polimi.ingsw.model.player.Player;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ActionTest {
+public class ActionTest {
 
     // TODO:
     @Test
-    void executeMove() {
+    public void testExecuteMove() throws InvalidMoveException, InvalidBuildException {
         Board board = new Board();
         Player p1 = new Player("p1");
         Player p2 = new Player("p2");
 
-        try {
-            board.putPawn(p1.getPawn(0), new Coordinate(1,3));
-            board.putPawn(p2.getPawn(0), new Coordinate(2,2));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        board.putPawn(p1.getPawn(0), new Coordinate(1,3));
+        board.putPawn(p2.getPawn(0), new Coordinate(2,2));
 
-        try {
-            // TODO:
-            Action move = new Action(ActionKind.MOVE);
-            move.execute(board, p1.getPawn(0), new Coordinate(2,2));
-        } catch (InvalidMoveException e) {
-            e.printStackTrace();
-        }
-
-
-
+        // TODO:
+        Action move = new Action(ActionKind.MOVE);
+        assertThrows(InvalidMoveException.class, () ->
+                move.execute(board, p1.getPawn(0), new Coordinate(2,2)));
     }
 
-    // TODO:
+    // TODO: change to public and write tests
     @Test
-    void checkAllowed() {
+    private void testCheckAllowed() {
+        throw new UnsupportedOperationException();
     }
 }
--- src/test/java/it/polimi/ingsw/model/board/BoardTest.java
+++ src/test/java/it/polimi/ingsw/model/board/BoardTest.java
@@ -9,54 +9,60 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
-
-    Board board = new Board();
-    Pawn pawn1;
-    Pawn pawn2;
-
     @Test
     public void testMovePawn() throws InvalidMoveException {
-        setupTestPawns();
-        Coordinate p0 = board.getPawnPosition(pawn1);
-        Coordinate p1 = new Coordinate(0,3);
-        board.movePawn(pawn1, p1);
+        // Set up board for test
+        Board board = new Board();
+        Pawn[] pawns = setupTestPawns(board);
 
-        assertEquals(p1, board.getPawnPosition(pawn1));
-        assertEquals(Optional.of(pawn1), board.getPawnAt(p1));
+        Coordinate p0 = board.getPawnPosition(pawns[0]);
+        Coordinate p1 = new Coordinate(0,3);
+        board.movePawn(pawns[0], p1);
+
+        assertEquals(p1, board.getPawnPosition(pawns[0]));
+        assertEquals(Optional.of(pawns[0]), board.getPawnAt(p1));
         assertEquals(Optional.empty(), board.getPawnAt(p0));
     }
 
     @Test
     public void testSwapPawn() {
-        setupTestPawns();
-        Coordinate p_p1 = board.getPawnPosition(pawn1);
-        Coordinate p_p2 = board.getPawnPosition(pawn2);
-        board.swapPawn(pawn1, pawn2);
+        // Set up board for test
+        Board board = new Board();
+        Pawn[] pawns = setupTestPawns(board);
 
-        assertEquals(p_p1, board.getPawnPosition(pawn2));
-        assertEquals(p_p2, board.getPawnPosition(pawn1));
+        Coordinate p_p1 = board.getPawnPosition(pawns[0]);
+        Coordinate p_p2 = board.getPawnPosition(pawns[1]);
+        board.swapPawn(pawns[0], pawns[1]);
 
-        assertEquals(Optional.of(pawn1), board.getPawnAt(p_p2));
-        assertEquals(Optional.of(pawn2), board.getPawnAt(p_p1));
+        assertEquals(p_p1, board.getPawnPosition(pawns[1]));
+        assertEquals(p_p2, board.getPawnPosition(pawns[0]));
+
+        assertEquals(Optional.of(pawns[0]), board.getPawnAt(p_p2));
+        assertEquals(Optional.of(pawns[1]), board.getPawnAt(p_p1));
     }
 
-    private void setupTestPawns() {
-        pawn1 = new Pawn(new Player("test_1"), 1);
-        pawn2 = new Pawn(new Player("test_2"), 1);
+    private Pawn[] setupTestPawns(Board board) {
+        Pawn[] pawns = new Pawn[2];
+        pawns[0] = new Pawn(new Player("test_1"), 0);
+        pawns[1] = new Pawn(new Player("test_2"), 1);
 
         Coordinate c1 = new Coordinate(1, 3);
         Coordinate c2 = new Coordinate(4,0);
         try {
-            board.putPawn(pawn1, c1);
-            board.putPawn(pawn2, c2);
+            board.putPawn(pawns[0], c1);
+            board.putPawn(pawns[1], c2);
         } catch (Exception e) {
             e.printStackTrace();
             fail();
         }
+
+        return pawns;
     }
 
     @Test
     public void testBuildBlock() throws InvalidBuildException{
+        Board board = new Board();
+
         Coordinate c = new Coordinate(1,2);
         assertEquals(Optional.empty(), board.getBuildingAt(c));
 
@@ -68,6 +74,8 @@ public class BoardTest {
 
     @Test
     public void testBuildDome() throws InvalidBuildException{
+        Board board = new Board();
+
         Coordinate c = new Coordinate(2,2);
         assertEquals(Optional.empty(), board.getBuildingAt(c));
 
@@ -81,19 +89,23 @@ public class BoardTest {
 
     @Test
     public void testPutPawn() throws InvalidMoveException {
+        Board board = new Board();
+
         Coordinate c1 = new Coordinate(0, 2);
-        pawn1 = new Pawn(new Player ("test_put"), 2);
+        Pawn pawn = new Pawn(new Player ("test_put"), 2);
 
         assertFalse(board.getPawnAt(c1).isPresent());
 
-        board.putPawn(pawn1, c1);
+        board.putPawn(pawn, c1);
         assertTrue(board.getPawnAt(c1).isPresent());
-        assertEquals(c1, board.getPawnPosition(pawn1));
-        assertEquals(Optional.of(pawn1), board.getPawnAt(c1));
+        assertEquals(c1, board.getPawnPosition(pawn));
+        assertEquals(Optional.of(pawn), board.getPawnAt(c1));
     }
 
     @Test
     public void testRemovePawn() throws InvalidMoveException {
+        Board board = new Board();
+
         Pawn p = new Pawn(new Player("test_delete"), 1);
         Coordinate c = new Coordinate(4,4);
 
