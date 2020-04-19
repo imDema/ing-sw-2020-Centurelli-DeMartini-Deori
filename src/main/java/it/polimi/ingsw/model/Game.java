7da9dd07957553f3be9e12cf39e67724c71eea6f
Nicolò <10572912@polimi.it>
2020-04-19 18:54:53 +0200

    Implemented Class Game and GameTest.

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,20 +1,64 @@
 package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.player.God;
 import it.polimi.ingsw.model.player.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    private List<Player> players;
+    private List<Player> players = new ArrayList<>();
     private Board board;
-    private int turn;
+    private int turn = 0;
+    private int eliminationTurn = turn;
+    private List<String> usernameList = new ArrayList<>();
+
+    public void addUsername(String username) {
+        usernameList.add(username);
+    }
+
+    public int getPlayerNumber() {
+        return players.size() ;
+    }
+
+    public Player getPlayer ( int i){
+        return players.get(i);
+    }
+
+    public int getTurn() {
+        return turn + 1;
+    }
+
+    public void elimination(Player player) throws InvalidStateException {
+        if (players.size() > 0) {
+            players.remove(player);
+            eliminationTurn = turn;
+            } else {
+                throw new InvalidStateException();
+            }
+        }
+
+
+    public void setPlayer(God god) throws InvalidStateException {
+        if (players.size() < 3) {
+            Player player = new Player(usernameList.get(players.size()));
+            player.setGod(god);
+            players.add(player);
+        } else {
+            throw new InvalidStateException();
+        }
+    }
 
     public void nextTurn() {
-        throw  new UnsupportedOperationException();
+        turn++;
     }
 
     public Player getCurrentPlayer() {
-        throw  new UnsupportedOperationException();
+        if (players.size() == 3) {
+            return players.get(turn % 3);
+        } else {
+            return players.get((turn - eliminationTurn) % 2);
+        }
     }
 }
---
+++ src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.action.Action;
+import it.polimi.ingsw.model.action.ActionFamily;
+import it.polimi.ingsw.model.action.Check;
+import it.polimi.ingsw.model.action.Effect;
+import it.polimi.ingsw.model.player.God;
+import it.polimi.ingsw.model.player.turnsequence.DefaultTurnSequence;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+class GameTest {
+
+    @Test
+    public void testElimination() throws InvalidStateException {
+        Game game = new Game();
+        Effect[] effect = new Effect[0];
+        Check[] check = new Check[0];
+        Action action = new Action("Prov", ActionFamily.MOVE, effect, check);
+        DefaultTurnSequence turnSequence = new DefaultTurnSequence(action, action);
+
+        String god_name1 = "God_Name1";
+        String god_name2 = "God_Name2";
+        String god_name3 = "God_Name3";
+
+        God god1 = new God("A", turnSequence);
+        God god2 = new God("B", turnSequence);
+        God god3 = new God("C", turnSequence);
+
+        game.addUsername(god_name1);
+        game.addUsername(god_name2);
+        game.addUsername(god_name3);
+
+        game.setPlayer(god1);
+        game.setPlayer(god2);
+        game.setPlayer(god3);
+
+        game.nextTurn();
+        assertEquals(2,game.getTurn());
+        assertEquals(3, game.getPlayerNumber());
+
+        game.elimination(game.getPlayer(1));
+        assertEquals(2, game.getPlayerNumber());
+
+        game.elimination(game.getCurrentPlayer());
+        assertEquals(1, game.getPlayerNumber());
+    }
+}
